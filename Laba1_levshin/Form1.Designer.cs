5078d05f40668c5c16dd40aed4f2d2cedf8b63d8
KorMIska <kormiska@gmail.com>
2023-11-20 08:14:54 +0300

    Добавьте файлы проекта.

---
+++ Laba1_levshin/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Laba1_levshin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(39, 423);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(1010, 46);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "OK";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(104, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "label1";
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(39, 42);
+            this.chart1.Name = "chart1";
+            series5.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.TileFlipX;
+            series5.BorderWidth = 7;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.MarkerBorderWidth = 3;
+            series5.MarkerSize = 77;
+            series5.Name = "Series1";
+            series6.BorderWidth = 7;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Series2";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(1010, 300);
+            this.chart1.TabIndex = 24;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(185, 387);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(45, 387);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1083, 600);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Laba1_levshin/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Laba1_levshin.Form1;
+
+namespace Laba1_levshin
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //public List<Itm> mod;
+        public Comand[] comand1;
+        public Comand[] comand2;
+
+        #region consts
+        public const int Tsh = 3;
+        public const int Fo = 5;
+        #endregion
+
+        #region модель
+        // 0 - черта 1 - декодировка 2 - вычесление 3 - упровление 4 - кеш
+        public struct Comand
+        {
+            public int t;
+            public bool kh;
+            public int type;   // 0 - черта 1 - декодировка 2 - вычесление 3 - упровление 4 - кеш
+            public int nomer;
+            public int time;
+
+            public Comand(int t, bool kh, int type)
+            {
+                this.t = t;
+                this.kh = kh;
+                this.type = type;
+                nomer = 0;
+                time = 0;
+            }
+            public Comand(int t, bool kh)
+            {
+                this.t = t;
+                this.kh = kh;
+                this.type = 0;
+                nomer = 0;
+                time = 0;
+            }
+        }
+
+        public struct Itm
+        {
+            public int nomer;
+            public int time;
+            public int type;
+            public int T;
+            public Itm(int nomer, int time, int type, int T)
+            {
+                this.nomer = nomer;
+                this.time = time;
+                this.type = type;
+                this.T = T;
+            }
+        }
+
+        public partial class K1
+        {
+            public int timeStop;
+            public int zadacha;
+
+
+
+            public K1()
+            {
+                timeStop = 0;
+                zadacha = 0;
+            }
+
+
+        }
+
+        public partial class KK
+        {
+            public int timeStop;
+
+            public KK()
+            {
+                timeStop = 0;
+            }
+
+
+
+        }
+
+        public partial class Model
+        {
+            KK kk = new KK();
+            K1 k1 = new K1();
+
+            List<Itm> queueKK = new List<Itm>();
+            List<Itm> queueK1 = new List<Itm>();
+
+            Comand[] cs;
+
+            public Model(Comand[] cs) { this.cs = cs; }
+
+            public int Rabot()
+            {
+                
+                int time = 0;
+
+                int countCom = 0;
+
+                //Один цыкл == Один такт
+
+                while (true)
+                {
+                    // Если конвеир свободен и нет заявок от кэш контролера
+                    // Обрабатывается новая команда
+                    if (k1.timeStop <= 0 & queueK1.Count == 0)
+                    {
+                        // countCom - номер команды
+                        // проверка того чтобы countCom не был больше количества команд
+                        // Что означает что все команды обработаны
+                        if (countCom == cs.Length)
+                        {
+                            //Доп проверка того что выполнены команды из КК
+                            if (queueK1.Count == 0 & queueKK.Count == 0)
+                            {
+                                return time;
+                            }
+                        }
+                        else
+                        {
+                            // Выполняется Декодировка
+
+                            if (cs[countCom].kh)  // Данные есть в кэше
+                            {
+                                // Кэшь занят на 1 цикл
+                                k1.timeStop = 1;
+
+                                queueK1.Insert(0, new Itm(countCom, time, cs[countCom].type, cs[countCom].t));
+
+
+                            }
+                            else // Кэш промах
+                            {
+
+                                // Отпрака запроса в КК
+                                queueKK.Add(new Itm(countCom, time, cs[countCom].type, cs[countCom].t));
+
+                                countCom++;
+                                // continue чтобы не защитало такт
+                                continue;
+                            }
+                            countCom++;
+
+                        }
+                    }
+
+                    // К1 свободен и есть заявка на работу
+                    if (k1.timeStop <= 0 & queueK1.Count != 0)
+                    {
+                        // type == уровление устройством 
+                        if (queueK1[0].type == 3)
+                        {
+                            // КК должен быть свободен 
+                            // Иначе Должен ждать 
+                            if (kk.timeStop <= 0)
+                            {
+                                Itm cur = queueK1[0];
+
+                                k1.timeStop = Tsh * cur.T;
+                                k1.zadacha = 3;
+
+                                queueK1.RemoveAt(0);
+                            }
+                        }
+                        else
+                        {
+                            if (queueK1[0].type == 2)
+                            {
+
+                                Itm cur = queueK1[0];
+
+                                queueK1.RemoveAt(0);
+
+                                k1.timeStop = 1 * cur.T;
+                            }
+                            else
+                            {
+                                if (queueK1[0].type == 1)
+                                {
+                                    k1.timeStop = 1;
+
+                                    queueK1.RemoveAt(0);
+                                }
+                            }
+                        }
+                    }
+                    // КК свободен и есть запрос
+                    if (kk.timeStop <= 0 & queueKK.Count != 0 & k1.zadacha != 3)
+                    {
+                        kk.timeStop = Tsh * Fo;
+                    }
+
+                    if (kk.timeStop - 1 == 0)
+                    {
+                        Itm cur = queueKK[0];
+
+                        cur.time = time;
+
+                        queueK1.Add(new Itm(cur.nomer, time, 1, 1));
+                        queueK1.Add(cur);
+
+                        queueKK.RemoveAt(0);
+                    }
+
+                    if (k1.timeStop - 1 == 0)
+                    {
+                        k1.zadacha = 0;
+                    }
+
+                    // Условный такт
+                    kk.timeStop--;
+                    k1.timeStop--;
+
+                    time++;
+
+                }
+
+
+
+            }
+        }
+
+
+        #endregion
+        #region интерфейс
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 1000; i += 10)
+            {
+
+                comand1 = new Comand[i];
+                comand2 = new Comand[i];
+
+
+                ////(int t, bool kh, int type)
+
+                for (int n = 0; n < i; n++)
+                {
+                    int t = 0;
+                    bool kh = false;
+                    int type = 0;
+
+                    int rn = rnd.Next(0, 100);
+
+                    if (rn >= 0 & rn < 20)
+                    {
+                        type = 2;
+
+                        int rn2 = rnd.Next(0, 100);
+
+                        if (rn2 >= 0 & rn2 < 70)
+                            t = 5;
+                        if (rn2 >= 70 & rn2 < 90)
+                            t = 2;
+                        if (rn2 >= 90 & rn2 < 100)
+                            t = 1;
+
+                    }
+                    if (rn >= 20 & rn < 35)
+                    {
+                        type = 2;
+
+                        int rn2 = rnd.Next(0, 100);
+
+                        if (rn2 >= 0 & rn2 < 70)
+                            t = 2;
+                        if (rn2 >= 70 & rn2 < 90)
+                            t = 5;
+                        if (rn2 >= 90 & rn2 < 100)
+                            t = 1;
+
+                    }
+                    if (rn >= 35 & rn < 50)
+                    {
+                        type = 3;
+
+                        int rn2 = rnd.Next(0, 100);
+
+                        if (rn2 >= 0 & rn2 < 80)
+                            t = 2;
+                        if (rn2 >= 80 & rn2 < 100)
+                            t = 1;
+                    }
+                    if (rn >= 50 & rn < 100)
+                    {
+                        type = 2;
+
+                        int rn2 = rnd.Next(0, 100);
+
+                        if (rn2 >= 0 & rn2 < 60)
+                            t = 2;
+                        if (rn2 >= 60 & rn2 < 100)
+                            t = 1;
+                    }
+
+                    rn = rnd.Next(0, 100);
+
+                    if (rn < 75)
+                    {
+                        kh = true;
+                    }
+                    else
+                        kh = false;
+
+                    comand1[n] = new Comand(t, kh, type);
+
+                    if (rn < 90)
+                    {
+                        kh = true;
+                    }
+                    else
+                        kh = false;
+
+                    comand2[n] = new Comand(t, kh, type);
+
+                }
+
+                Model m1 = new Model(comand1);
+                Model m2 = new Model(comand2);
+
+                chart1.Series[0].Points.AddXY(i, m1.Rabot());
+                chart1.Series[1].Points.AddXY(i, m2.Rabot());
+
+            }
+        }
+        #endregion
+    }
+}
+
+  
+
